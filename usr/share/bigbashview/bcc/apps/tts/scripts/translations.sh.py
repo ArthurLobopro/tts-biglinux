34d16863c6a1908bc2a7ce6e65f318a6f275fa6a
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2025-02-18 07:00:36 -0300

    Update translations.sh.py

--- usr/share/bigbashview/bcc/apps/tts/scripts/translations.sh.py
+++ usr/share/bigbashview/bcc/apps/tts/scripts/translations.sh.py
@@ -13,14 +13,14 @@ lang_translations.install()
 
 
 
-ARGS = argv[1]
+if __name__ == "__main__":
+    ARGS = argv[1]
 
-strings_to_return: list[str] = json.loads(ARGS)
+    strings_to_return: list[str] = json.loads(ARGS)
 
-TRANSLATIONS = {}
+    TRANSLATIONS = {}
 
-for string in strings_to_return:
-    TRANSLATIONS[string] = lang_translations.gettext(string)
+    for string in strings_to_return:
+        TRANSLATIONS[string] = lang_translations.gettext(string)
 
-print(json.dumps(TRANSLATIONS))
-# print("Teste")
+    print(json.dumps(TRANSLATIONS))
